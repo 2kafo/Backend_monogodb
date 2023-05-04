d97204e082d67ecf938b746ffc50c3a3debba79c
2kafo <akafonogo21@gmail.com>
2023-05-05 00:10:52 +0300

    small changes

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const To_do_list = require('./models/to_do_list');
+const user = require('./models/user');
+
 
 //DATABASE CONNECTION
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 mongoose.set('strictQuery', false);
 
@@ -20,26 +23,61 @@ const connectDB = async () => {
 }
 
 //ROUTES CONNECTION(API)
+
+//landing route
 app.get('/', (req, res) => {
-    res.send({ title: 'To_do_list' });
+    res.send("Put url for front-end");
 });
 
-//api for inserting to do list
+
+//API FOR MANUPULATION OF USERS
+
+//insert user
+app.post('/user-create', async(req, res) => {
+    try {
+        await user.insertMany([
+            {
+                fullname:"Amani Kafonogo",
+                username:"technical@assesment.com",
+                password:"1234"
+            }
+        ])
+        res.send("User created")
+    } catch (error) {
+        console.log('err', error);
+    }
+});
+
+//Auth login users
+app.get('/user', (req, res) =>{
+    res.send(' user login user')
+});
+
+//create a new user
+app.post('/user-save', async (req, res) => {
+    try {
+        res.send('Task_to_do_create_user')
+    } catch (error) {
+        console.log('err', error);
+    }
+});
+
+
+
+//API FOR TO DO LIST MANIPULATION
+
+//insert deafult to do task to user loged in
 app.get('/add-task', async (req, res) => {
     try {
         await To_do_list.insertMany([
             {
                 name: "Get technical assessment started",
                 status: "Not completed"
-            },
-            {
-                name: "backend started",
-                status: "Not completed"
             }
         ])
         res.send('Task added');
     } catch (error) {
-        console.log('err', error)
+        console.log('err', error);
     }
 });
 
