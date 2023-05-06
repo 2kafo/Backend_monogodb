99c0a1599ce67f39999f301f7a67e8041d59503d
2kafo <akafonogo21@gmail.com>
2023-05-06 22:30:04 +0300

    user modified

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose');
-const userRoutes = require("./routes/users");
+const userRoutes = require("./routes/user");
 const authRoutes = require("./routes/auth");
 
 const To_do_list = require('./models/to_do_list');
