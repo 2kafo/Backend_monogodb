b16a7f30ebebb3ea16118b8f1264244cc9453fb5
2kafo <akafonogo21@gmail.com>
2023-05-06 23:32:21 +0300

    user modified

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-import { user, validate } from "../models/user";
+import user from "../models/user";
+import validate  from "../models/user";
 import { genSalt, hash } from "bcrypt";
 
 router.post("/user-create", async (req, res) => {
